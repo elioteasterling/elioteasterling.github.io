274a9c0eb36166617007868efda303cb8f429b9d
elioteasterling <115996022+elioteasterling@users.noreply.github.com>
2022-12-21 18:25:37 -0500

    copy paste carelessness

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
+    site: 'https://elioteasterling.github.io',
     base: '/elioteasterling.github.io',
 })
